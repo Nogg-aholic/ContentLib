48115167b5b705223e9e57b746c4a4e5278785ec
Angel Pons <th3fanbus@gmail.com>
2025-08-17 20:59:28 +0200

    Misc clean-up: CLRecipeBPFLib
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- Source/ContentLib/Private/CLRecipeBPFLib.cpp
+++ Source/ContentLib/Private/CLRecipeBPFLib.cpp
@@ -18,11 +18,10 @@
 
 void UCLRecipeBPFLib::InitRecipeFromStruct(UContentLibSubsystem* Subsystem ,FContentLib_Recipe RecipeStruct, TSubclassOf<class UFGRecipe> Recipe,bool ClearIngredients,bool ClearProducts,bool ClearBuilders)
 {
-	if (!Recipe) {
+	UFGRecipe* CDO = Recipe.GetDefaultObject();
+	if (!IsValid(CDO)) {
 		return;
 	}
-
-	UFGRecipe* CDO = Recipe.GetDefaultObject();
 
 	// If a Name is specified, it will also turn on override
 	if (RecipeStruct.Name != "") {
@@ -36,31 +35,33 @@ void UCLRecipeBPFLib::InitRecipeFromStruct(UContentLibSubsystem* Subsystem ,FCon
 		CDO->mDisplayNameOverride = false;
 	}
 
-	if (RecipeStruct.Category != "")
-	{
-		const TSubclassOf<UFGItemCategory> Out = UBPFContentLib::SetCategoryWithLoad(RecipeStruct.Category, Subsystem, false);
-		if (Out) {
+	if (RecipeStruct.Category != "") {
+		if (const TSubclassOf<UFGItemCategory> Out = UBPFContentLib::SetCategoryWithLoad(RecipeStruct.Category, Subsystem, false)) {
 			CDO->mOverriddenCategory = Out;
 		}
 		if (!CDO->mOverriddenCategory) {
 			UE_LOG(LogContentLib, Error, TEXT("Recipe Category Override probably failed; a category was specified in the struct but was still nullptr after apply"));
 		}
 	}
-	UBPFContentLib::AddToItemAmountArray(Recipe.GetDefaultObject()->mIngredients,RecipeStruct.Ingredients,Subsystem->mItems,ClearIngredients);
-	UBPFContentLib::AddToItemAmountArray(Recipe.GetDefaultObject()->mProduct,RecipeStruct.Products,Subsystem->mItems,ClearProducts);
-	AddBuilders(Recipe,RecipeStruct,Subsystem->mBuilders,Subsystem->mCraftingComps, ClearBuilders);
-	AddToSchematicUnlock(Recipe,RecipeStruct,Subsystem);
-	if(RecipeStruct.ManufacturingDuration != -1)
+	UBPFContentLib::AddToItemAmountArray(CDO->mIngredients, RecipeStruct.Ingredients, Subsystem->mItems, ClearIngredients);
+	UBPFContentLib::AddToItemAmountArray(CDO->mProduct, RecipeStruct.Products, Subsystem->mItems, ClearProducts);
+	AddBuilders(Recipe, RecipeStruct, Subsystem->mBuilders, Subsystem->mCraftingComps, ClearBuilders);
+	AddToSchematicUnlock(Recipe, RecipeStruct, Subsystem);
+	if (RecipeStruct.ManufacturingDuration != -1) {
 		CDO->mManufactoringDuration = RecipeStruct.ManufacturingDuration;
-	if (RecipeStruct.ManualManufacturingMultiplier != -1)
+	}
+	if (RecipeStruct.ManualManufacturingMultiplier != -1) {
 		CDO->mManualManufacturingMultiplier = RecipeStruct.ManualManufacturingMultiplier;
-	if (RecipeStruct.VariablePowerConsumptionConstant != -1)
+	}
+	if (RecipeStruct.VariablePowerConsumptionConstant != -1) {
 		CDO->mVariablePowerConsumptionConstant = RecipeStruct.VariablePowerConsumptionConstant;
-	if (RecipeStruct.VariablePowerConsumptionFactor != -1)
+	}
+	if (RecipeStruct.VariablePowerConsumptionFactor != -1) {
 		CDO->mVariablePowerConsumptionFactor = RecipeStruct.VariablePowerConsumptionFactor;
-	if (RecipeStruct.ManufacturingMenuPriority != -1)
+	}
+	if (RecipeStruct.ManufacturingMenuPriority != -1) {
 		CDO->mManufacturingMenuPriority = RecipeStruct.ManufacturingMenuPriority;
-
+	}
 }
 
 bool UCLRecipeBPFLib::AddBuilder(FString builderName, UFGRecipe* recipeCDO, TArray<UClass*> AllKnownBuilders, TArray<UClass*> AllKnownCraftingComps) {
@@ -119,31 +120,32 @@ bool UCLRecipeBPFLib::AddBuilder(FString builderName, UFGRecipe* recipeCDO, TArr
 
 void UCLRecipeBPFLib::AddBuilders(const TSubclassOf<class UFGRecipe> Recipe, FContentLib_Recipe RecipeStruct, TArray<UClass*> AllKnownBuilders, TArray<UClass*> AllKnownCraftingComps, const bool ClearFirst)
 {
-	if (!Recipe)
-		return;
 	auto recipeCDO = Recipe.GetDefaultObject();
-	if (ClearFirst)
+	if (!IsValid(recipeCDO)) {
+		return;
+	}
+	if (ClearFirst) {
 		recipeCDO->mProducedIn.Empty();
+	}
 
 	for(FString& requested : RecipeStruct.BuildIn) {
 		AddBuilder(requested, recipeCDO, AllKnownBuilders, AllKnownCraftingComps);
 	}
 }
 
-void UCLRecipeBPFLib::AddToSchematicUnlock(const TSubclassOf<class UFGRecipe> Recipe,FContentLib_Recipe RecipeStruct, UContentLibSubsystem* Subsystem)
+void UCLRecipeBPFLib::AddToSchematicUnlock(const TSubclassOf<class UFGRecipe> Recipe, FContentLib_Recipe RecipeStruct, UContentLibSubsystem* Subsystem)
 {
 	if (!Recipe) {
 		return;
 	}
 	for (const FString& SchematicToFind : RecipeStruct.UnlockedBy) {
-		UClass * SchematicClass = UBPFContentLib::FindClassWithLog(SchematicToFind,UFGSchematic::StaticClass(),Subsystem);
-		if (SchematicClass) {
+		if (UClass* SchematicClass = UBPFContentLib::FindClassWithLog(SchematicToFind, UFGSchematic::StaticClass(), Subsystem)) {
 			UBPFContentLib::AddRecipeToUnlock(SchematicClass, Subsystem, Recipe);
 		} else {
 			UE_LOG(LogContentLib, Error, TEXT("CL Recipes: Failed to find Schematic for UnlockedBy with name %s for Recipe %s"), *SchematicToFind, *Recipe->GetName());
 		}
 	}
-};
+}
 
 
 FContentLib_Recipe UCLRecipeBPFLib::GenerateCLRecipeFromString(FString String)
@@ -176,14 +178,15 @@ FContentLib_Recipe UCLRecipeBPFLib::GenerateCLRecipeFromString(FString String)
 	UBPFContentLib::SetFloatFieldWithLog(Recipe.VariablePowerConsumptionFactor, "VariablePowerConsumptionFactor", ParsedJson);
 	UBPFContentLib::SetFloatFieldWithLog(Recipe.VariablePowerConsumptionConstant, "VariablePowerConsumptionConstant", ParsedJson);
 	UBPFContentLib::SetFloatFieldWithLog(Recipe.ManufacturingMenuPriority, "ManufacturingMenuPriority", ParsedJson);
-	if (!UBPFContentLib::SetStringIntMapFieldWithLog(Recipe.Ingredients, "Ingredients", ParsedJson))
+	if (!UBPFContentLib::SetStringIntMapFieldWithLog(Recipe.Ingredients, "Ingredients", ParsedJson)) {
 		Recipe.ClearIngredients = false;
-	if (!UBPFContentLib::SetStringIntMapFieldWithLog(Recipe.Products, "Products", ParsedJson))
+	}
+	if (!UBPFContentLib::SetStringIntMapFieldWithLog(Recipe.Products, "Products", ParsedJson)) {
 		Recipe.ClearProducts = false;
-
-	if (!UBPFContentLib::SetStringArrayFieldWithLog(Recipe.BuildIn, "ProducedIn", ParsedJson))
+	}
+	if (!UBPFContentLib::SetStringArrayFieldWithLog(Recipe.BuildIn, "ProducedIn", ParsedJson)) {
 		Recipe.ClearBuilders = false;
-	
+	}
 	UBPFContentLib::SetStringArrayFieldWithLog(Recipe.UnlockedBy, "UnlockedBy", ParsedJson);
 	UBPFContentLib::SetBooleanFieldWithLog(Recipe.ClearIngredients, "ClearIngredients", ParsedJson);
 	UBPFContentLib::SetBooleanFieldWithLog(Recipe.ClearProducts, "ClearProducts", ParsedJson);
@@ -195,9 +198,10 @@ FContentLib_Recipe UCLRecipeBPFLib::GenerateCLRecipeFromString(FString String)
 
 FString UCLRecipeBPFLib::SerializeRecipe(const TSubclassOf<UFGRecipe> Recipe)
 {
-	if (!Recipe)
+	const auto CDO = Recipe.GetDefaultObject();
+	if (!IsValid(CDO)) {
 		return "";
-	const auto CDO = Cast<UFGRecipe>(Recipe->GetDefaultObject());
+	}
 	const auto Obj = MakeShared<FJsonObject>();
 	const auto Name = MakeShared<FJsonValueString>(CDO->mDisplayName.ToString());
 	const auto Override = MakeShared<FJsonValueBoolean>(CDO->mDisplayNameOverride);
@@ -206,26 +210,26 @@ FString UCLRecipeBPFLib::SerializeRecipe(const TSubclassOf<UFGRecipe> Recipe)
 	const auto mVariablePowerConsumptionFactor = MakeShared<FJsonValueNumber>(CDO->mVariablePowerConsumptionFactor);
 	const auto mVariablePowerConsumptionConstant = MakeShared<FJsonValueNumber>(CDO->mVariablePowerConsumptionConstant);
 
-	TArray< TSharedPtr<FJsonValue>> Ingredients;
-	TArray< TSharedPtr<FJsonValue>> Products;
-	TArray< TSharedPtr<FJsonValue>> ProducedIn; 
-	for(auto& i : CDO->mIngredients) {
+	TArray<TSharedPtr<FJsonValue>> Ingredients;
+	TArray<TSharedPtr<FJsonValue>> Products;
+	TArray<TSharedPtr<FJsonValue>> ProducedIn; 
+	for (auto& i : CDO->mIngredients) {
 		auto IngObj = MakeShared<FJsonObject>();
 		IngObj->Values.Add("Item",MakeShared<FJsonValueString>(i.ItemClass->GetName()));
 		IngObj->Values.Add("Amount",MakeShared<FJsonValueNumber>(i.Amount));
 		Ingredients.Add(MakeShared<FJsonValueObject>(IngObj));
 	}
-	for(auto& i : CDO->mProduct) {
+	for (auto& i : CDO->mProduct) {
 		auto IngObj = MakeShared<FJsonObject>();
 		IngObj->Values.Add("Item",MakeShared<FJsonValueString>(i.ItemClass->GetName()));
 		IngObj->Values.Add("Amount",MakeShared<FJsonValueNumber>(i.Amount));
 		Products.Add(MakeShared<FJsonValueObject>(IngObj));
 	}
-	for(auto& i : CDO->mProducedIn) {
-		if (!i) {
-			UE_LOG(LogContentLib, Error, TEXT("Encountered NULL producer in recipe '%s'"), *CDO->GetPathName());
-		} else {
+	for (auto& i : CDO->mProducedIn) {
+		if (i) {
 			ProducedIn.Add(MakeShared<FJsonValueString>(i->GetPathName()));
+		} else {
+			UE_LOG(LogContentLib, Error, TEXT("Encountered NULL producer in recipe '%s'"), *CDO->GetPathName());
 		}
 	}
 	const auto Ing = MakeShared<FJsonValueArray>(Ingredients);
@@ -257,23 +261,22 @@ FString UCLRecipeBPFLib::SerializeCLRecipe(FContentLib_Recipe Recipe)
 	const auto mVariablePowerConsumptionFactor = MakeShared<FJsonValueNumber>(Recipe.VariablePowerConsumptionFactor);
 	const auto mVariablePowerConsumptionConstant = MakeShared<FJsonValueNumber>(Recipe.VariablePowerConsumptionConstant);
 
-	
-	TArray< TSharedPtr<FJsonValue>> Ingredients;
-	TArray< TSharedPtr<FJsonValue>> Products;
-	TArray< TSharedPtr<FJsonValue>> ProducedIn; 
-	for(auto& i : Recipe.Ingredients) {
+	TArray<TSharedPtr<FJsonValue>> Ingredients;
+	TArray<TSharedPtr<FJsonValue>> Products;
+	TArray<TSharedPtr<FJsonValue>> ProducedIn; 
+	for (auto& i : Recipe.Ingredients) {
 		auto IngObj = MakeShared<FJsonObject>();
 		IngObj->Values.Add("Item",MakeShared<FJsonValueString>(i.Key));
 		IngObj->Values.Add("Amount",MakeShared<FJsonValueNumber>(i.Value));
 		Ingredients.Add(MakeShared<FJsonValueObject>(IngObj));
 	}
-	for(auto& i : Recipe.Products) {
+	for (auto& i : Recipe.Products) {
 		auto IngObj = MakeShared<FJsonObject>();
 		IngObj->Values.Add("Item",MakeShared<FJsonValueString>(i.Key));
 		IngObj->Values.Add("Amount",MakeShared<FJsonValueNumber>(i.Value));
 		Products.Add(MakeShared<FJsonValueObject>(IngObj));
 	}
-	for(auto& i : Recipe.BuildIn) {
+	for (auto& i : Recipe.BuildIn) {
 		ProducedIn.Add(MakeShared<FJsonValueString>(i));
 	}
 	const auto Ing = MakeShared<FJsonValueArray>(Ingredients);
@@ -284,9 +287,6 @@ FString UCLRecipeBPFLib::SerializeCLRecipe(FContentLib_Recipe Recipe)
 	const auto ClearBuilders = MakeShared<FJsonValueBoolean>(Recipe.ClearBuilders);
 	const auto RequireItemValidation = MakeShared<FJsonValueBoolean>(Recipe.RequireItemValidation);
 
-
-
-	
 	Obj->Values.Add("Name",Name);
 	Obj->Values.Add("OverrideName", Override);
 	Obj->Values.Add("Category", Cat);
@@ -305,4 +305,4 @@ FString UCLRecipeBPFLib::SerializeCLRecipe(FContentLib_Recipe Recipe)
 	const TSharedRef<TJsonWriter<TCHAR, TPrettyJsonPrintPolicy<TCHAR>>> JsonWriter = TJsonWriterFactory<TCHAR, TPrettyJsonPrintPolicy<TCHAR>>::Create(&Write); //Our Writer Factory
 	FJsonSerializer::Serialize(Obj, JsonWriter);
 	return Write;
-};
+}
